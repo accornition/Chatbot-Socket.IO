6c4ce2964e95dc1302319ec45b99593f274d0f2b
U-RAMA-LENOVA-IDE\vijay <accornition@github.com>
2020-06-06 15:28:53 +0530

    Fix wsgi server issue

--- chatbox_socketio/wsgi.py
+++ chatbox_socketio/wsgi.py
@@ -15,6 +15,13 @@ import socketio
 from chatbox.views import sio
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbox_socketio.settings")
-
 django_app = get_wsgi_application()
-application = socketio.WSGIApp(sio, django_app)
+application = socketio.Middleware(sio, django_app)
+
+import eventlet
+import eventlet.wsgi
+eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
+
+
+#django_app = get_wsgi_application()
+#application = socketio.WSGIApp(sio, django_app)
